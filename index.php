e27e3ccfe13fbb813960130644192cb141cccaab
Roman <romanrus98@mail.ru>
2021-12-06 21:18:52 +0500

    Сверстал хедер и немного тела

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="assets/css/default.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>Device</title>
+</head>
+<body>
+    <div class="container">
+    <?php 
+        include "assets/blocks/header.html"
+    ?>
+    <main class="main">
+        <div class="intro">
+            <div class="intro__info">
+                <div class="intro__title">
+                    Покупай модную технику
+                </div>
+                <div class="intro__subtitle">
+                Дешево, и без затруднений! 
+                Вы найдете большой выбор товаров, на которые всегда идут скидки!
+                </div>
+                <div class="intro__btn">
+                    <a class="intro__link" href="#">Продолжить</a>
+                </div>
+            </div>
+            <div class="intro__prev">
+                <div class="wrapper"></div>
+            </div>
+        </div>
+    </main>
+    </div>
+</body>
+</html>
